a236a6ca65bd1763afbf97387df99375898f07b7
Sirajul <161524666+sirajul9988@users.noreply.github.com>
2026-02-20 22:19:49 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/YOUR_API_KEY",
+      accounts: ["YOUR_PRIVATE_KEY"]
+    }
+  }
+};
